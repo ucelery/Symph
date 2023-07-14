e84500e21bbb0c2885e00b0af17540932a5c46aa
nokori <69152061+ucelery@users.noreply.github.com>
2023-07-14 23:11:51 +0800

    Implement Upload Song Functionality

--- src/main/java/Models/Database.java
+++ src/main/java/Models/Database.java
@@ -9,33 +9,40 @@ import com.mongodb.client.gridfs.GridFSBuckets;
 
 // Song Database
 import com.cloudinary.*;
+import com.cloudinary.utils.ObjectUtils;
+import com.mongodb.client.MongoCollection;
 
 import io.github.cdimascio.dotenv.Dotenv;
+import java.io.File;
+import java.util.Map;
 import java.util.concurrent.Executors;
 import java.util.concurrent.ThreadPoolExecutor;
+import org.bson.Document;
 
 public class Database {    
     MongoClient mongoClient;
+    MongoDatabase database;
     GridFSBucket gridFSBucket;
+    Cloudinary cloudinary;
+    
+    ThreadPoolExecutor executor = (ThreadPoolExecutor)Executors.newFixedThreadPool(10);
     int _threadCount = 0;
-
+    
     public Database() {  
         Dotenv dotenv = Dotenv.load();
-        
-        ThreadPoolExecutor executor = (ThreadPoolExecutor)Executors.newFixedThreadPool(10);
         
         // execute MongoDB thread
         executor.submit(() -> {
             _threadCount++;
             System.out.println("[ LOADING ] MongoDB");
             mongoClient = MongoClients.create("mongodb+srv://" + dotenv.get("MONGO_USERNAME") + ":" + dotenv.get("MONGO_PASSWORD") + "@cluster0.xvzkkrp.mongodb.net/?retryWrites=true&w=majority");
-            MongoDatabase database = mongoClient.getDatabase("Symph");
+            database = mongoClient.getDatabase("Symph");
             gridFSBucket = GridFSBuckets.create(database, "songs");
             
             System.out.println("[ READY ] MongoDB");
             _threadCount--;
-            
-            checkThreads();
+
+            invokeExecutorFinish("[ APP ] Application is Ready!");
             return null;
         });
         
@@ -43,23 +50,55 @@ public class Database {
         executor.submit(() -> {
             _threadCount++;
             System.out.println("[ LOADING ] Cloudinary");
-            Cloudinary cloudinary = new Cloudinary("cloudinary://" + dotenv.get("CLOUDINARY_API_KEY") + ":" + dotenv.get("CLOUDINARY_API_SECRET") + "@" + dotenv.get("CLOUDINARY_NAME"));
+
+            cloudinary = new Cloudinary("cloudinary://" + dotenv.get("CLOUDINARY_API_KEY") + ":" + dotenv.get("CLOUDINARY_API_SECRET") + "@" + dotenv.get("CLOUDINARY_NAME"));
             cloudinary.config.secure = true;
             
             System.out.println("[ READY ] Cloudinary");
             _threadCount--;
             
-            checkThreads();
+            invokeExecutorFinish("[ APP ] Application is Ready!");
+            return null;
+        });
+    }
+    
+    private boolean invokeExecutorFinish(String msg) {
+        if (msg != null && _threadCount <= 0)
+            System.out.println(msg);
+        
+        return _threadCount <= 0;
+    }
+    
+    public void insertSong(Song song) {
+        File audioFile = song.audioFile;
+        File coverFile = song.coverFile;
+        
+        // Upload the song and image cover
+        executor.submit(() -> {
+            Map<?, ?> audioRes = cloudinary.uploader().upload(audioFile, ObjectUtils.asMap(
+                    "resource_type", "video",
+                    "use_filename", true,
+                    "unique_filename", false,
+                    "overwrite", true
+            ));
+            
+            Map<?, ?> imageRes = cloudinary.uploader().upload(coverFile, ObjectUtils.asMap(
+                "use_filename", true,
+                "unique_filename", false,
+                "overwrite", true
+            ));
+            
+            // Upload details to mongo
+            MongoCollection<Document> collection = database.getCollection("songs");
+            Document doc = new Document("artist", song.artist)
+                    .append("title", song.title)
+                    .append("imageURL", imageRes.get("secure_url"))
+                    .append("audioURL", audioRes.get("secure_url"));
+
+            collection.insertOne(doc);
             return null;
         });
         
-        
-    }
-    
-    private void checkThreads() {
-        if (_threadCount <= 0) {
-            // Ready
-            System.out.println("[ APP ] App is Ready!");
-        }
+
     }
 }
