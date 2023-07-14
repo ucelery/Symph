b75d0c97456c7aba52605d8a908d35be7f737790
nokori <69152061+ucelery@users.noreply.github.com>
2023-07-14 17:06:30 +0800

    Implement Multithreading

--- src/main/java/Models/Database.java
+++ src/main/java/Models/Database.java
@@ -17,7 +17,8 @@ import java.util.concurrent.ThreadPoolExecutor;
 public class Database {    
     MongoClient mongoClient;
     GridFSBucket gridFSBucket;
-    
+    int _threadCount = 0;
+
     public Database() {  
         Dotenv dotenv = Dotenv.load();
         
@@ -25,23 +26,40 @@ public class Database {
         
         // execute MongoDB thread
         executor.submit(() -> {
+            _threadCount++;
             System.out.println("[ LOADING ] MongoDB");
             mongoClient = MongoClients.create("mongodb+srv://" + dotenv.get("MONGO_USERNAME") + ":" + dotenv.get("MONGO_PASSWORD") + "@cluster0.xvzkkrp.mongodb.net/?retryWrites=true&w=majority");
             MongoDatabase database = mongoClient.getDatabase("Symph");
             gridFSBucket = GridFSBuckets.create(database, "songs");
             
             System.out.println("[ READY ] MongoDB");
+            _threadCount--;
+            
+            checkThreads();
             return null;
         });
         
         // execute Cloudinary thread
         executor.submit(() -> {
+            _threadCount++;
             System.out.println("[ LOADING ] Cloudinary");
             Cloudinary cloudinary = new Cloudinary("cloudinary://" + dotenv.get("CLOUDINARY_API_KEY") + ":" + dotenv.get("CLOUDINARY_API_SECRET") + "@" + dotenv.get("CLOUDINARY_NAME"));
             cloudinary.config.secure = true;
             
             System.out.println("[ READY ] Cloudinary");
+            _threadCount--;
+            
+            checkThreads();
             return null;
-        });        
+        });
+        
+        
+    }
+    
+    private void checkThreads() {
+        if (_threadCount <= 0) {
+            // Ready
+            System.out.println("[ APP ] App is Ready!");
+        }
     }
 }
