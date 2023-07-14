4d0231c2ea210c95816338d98edd91388374a43c
nokori <69152061+ucelery@users.noreply.github.com>
2023-07-14 21:39:05 +0800

    Add Upload Song Feature

--- src/main/java/Controllers/Controller.java
+++ src/main/java/Controllers/Controller.java
@@ -1,5 +1,50 @@
 package Controllers;
 
+import Models.Database;
+import Models.Song;
+import java.io.File;
+import java.util.ArrayList;
+import javafx.scene.media.Media;
+import javafx.scene.media.MediaPlayer;
+import javax.swing.JFileChooser;
+import javax.swing.JOptionPane;
+import javax.swing.filechooser.FileNameExtensionFilter;
+
 public class Controller {
+    Database db = new Database();
     
+    public Controller() {
+        
+    }
+    
+    public void uploadSong(Song song) {
+        ArrayList<String> errorStr = new ArrayList<String>();
+        
+        // Verify Input Fields
+        if (song.getArtist() == null || song.getArtist() == "")
+            errorStr.add("Artist");
+        
+        if (song.getTitle() == null || song.getTitle() == "")
+            errorStr.add("Title");
+        
+        if (song.getAudioFile() == null)
+            errorStr.add("Audio File");
+        
+        if (song.getCoverFile() == null)
+            errorStr.add("Cover File");
+        
+        if (!errorStr.isEmpty()) {
+            JOptionPane.showMessageDialog(null, "Please fill in the ff. fields: " + errorStr.toString(), "Invalid Input", JOptionPane.WARNING_MESSAGE);
+            return;
+        }   
+        
+        JOptionPane.showMessageDialog(null, "Upload Complete!" + errorStr.toString(), "Success!", JOptionPane.INFORMATION_MESSAGE);
+        db.insertSong(song);
+    }
+    
+    public void playAudio(String cloudinaryURL) {
+        Media media = new Media(cloudinaryURL);
+        MediaPlayer mediaPlayer = new MediaPlayer(media);
+        mediaPlayer.play();
+    }
 }
---
+++ src/main/java/Models/Song.java
@@ -0,0 +1,83 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package Models;
+
+import java.io.File;
+import javax.swing.JFileChooser;
+import javax.swing.filechooser.FileNameExtensionFilter;
+
+/**
+ *
+ * @author atond
+ */
+public class Song {
+    String id;
+    int localId;
+    String title;
+    String artist;
+    
+    File audioFile;
+    File coverFile;
+    
+    public void setTitle(String title) {
+        this.title = title;
+    }
+    
+    public String getTitle() {
+        return this.title;
+    }
+    
+    public void setCoverFile(File coverFile) {
+        this.coverFile = coverFile;
+    }
+    
+    public void setCoverFile() {
+        JFileChooser fileChooser = new JFileChooser();
+        fileChooser.setDialogTitle("Select Image File");
+
+        fileChooser.setAcceptAllFileFilterUsed(false);
+        FileNameExtensionFilter imgFilter0 = new FileNameExtensionFilter("Image Files", "png", "jpg");
+
+        fileChooser.addChoosableFileFilter(imgFilter0);
+            
+
+        if (fileChooser.showOpenDialog(null) == JFileChooser.APPROVE_OPTION) {
+            coverFile = fileChooser.getSelectedFile();
+        }
+    }
+    
+    public File getCoverFile() {
+        return this.coverFile;
+    }
+    
+    public void setAudioFile(File audioFile) {
+        this.audioFile = audioFile;
+    }
+    
+    public void setAudioFile() {
+        JFileChooser fileChooser = new JFileChooser();
+        fileChooser.setDialogTitle("Select MP3 File");
+
+        fileChooser.setAcceptAllFileFilterUsed(false);
+        FileNameExtensionFilter filter = new FileNameExtensionFilter("Audio Files", "mp3");
+        fileChooser.addChoosableFileFilter(filter);
+
+        if (fileChooser.showOpenDialog(null) == JFileChooser.APPROVE_OPTION && fileChooser.getSelectedFile() != null) {
+            this.audioFile = fileChooser.getSelectedFile();
+        }
+    }
+    
+    public File getAudioFile() {
+        return this.audioFile;
+    }
+    
+    public void setArtist(String artist) {
+        this.artist = artist;
+    }
+    
+    public String getArtist() {
+        return this.artist;
+    }
+}
